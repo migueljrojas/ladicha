20edb60929ee9540bd91abfcc87fbaf7a30fac36
Mike <migueljrojas@gmail.com>
2019-07-16 16:13:21 -0300

    prev

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,15 +3,15 @@
 
 'use strict';
 
-global.$ = global.jQuery = require('jquery');
-global._ = require('underscore');
+global.$ = global.jQuery;
+//global._ = require('underscore');
 var Header = require('../_modules/header/header');
 var Slider = require('../_modules/slider/slider');
 var Carta = require('./carta');
 var Reserva = require('./reserva');
 
 $(function() {
-    require('../../bower_components/slick-carousel/slick/slick');
+    //require('../../bower_components/slick-carousel/slick/slick');
 
     new Header();
     new Slider();

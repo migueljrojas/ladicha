641db33edf7afb90c986b0718802705d5c0160e1
Your Name <you@example.com>
2019-07-17 19:07:26 -0300

    Updates

--- src/_scripts/load-mesas.php
+++ src/_scripts/load-mesas.php
@@ -5,9 +5,9 @@
     $selectedDate = $_GET['selectedDate'];
 
     if ($con) {
-        $sql = mysqli_query($con, "SELECT * FROM reservas WHERE fecha = $selectedDate ORDER BY id DESC");
+        $sql = mysqli_query($con, "SELECT * FROM mesas WHERE fecha = $selectedDate");
         $rows = mysqli_fetch_all($sql, MYSQLI_ASSOC);
-
+        
         echo json_encode($rows);
     } else {
         echo "Error with database: " . $con->error;
